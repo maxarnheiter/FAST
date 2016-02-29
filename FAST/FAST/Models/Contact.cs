85efa17f9019f7623348cf80968e95418a273898
maxarnheiter <maxarnheiter@gmail.com>
2016-02-29 12:43:48 -0500

    1. Basic Models and Controllers

---
+++ FAST/FAST/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FAST.Models
+{
+    public class Contact
+    {
+
+        public int ID { get; set; }
+
+        [Display(Name="First Name")]
+        public string FirstName;
+
+        [Display(Name = "Last Name")]
+        public string LastName;
+
+
+    }
+}
---
+++ FAST/FAST/Models/Group.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FAST.Models
+{
+    public class Group
+    {
+
+        public int ID { get; set; }
+
+        public string Name;
+
+    }
+}
